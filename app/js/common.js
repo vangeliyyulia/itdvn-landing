18a4b9702d21040739cb91d4b8c1e4171ac419d9
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-09-15 22:17:30 +0300

    Some JavaScript added

---
+++ app/js/common.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("form-container").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("form-container").style.display = "none";
+}
